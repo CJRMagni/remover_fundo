73bbcf3acfb50897584b5fc39217bcb81e321761
CJRMagni <56973756+CJRMagni@users.noreply.github.com>
2025-04-21 13:49:27 -0300

    Atulização
    Versão 1

---
+++ main.py
@@ -0,0 +1,10 @@
+from rembg import remove
+from PIL import Image
+
+caminho_entrada = 'image3.jpg'
+caminho_saida = 'image1_ok.png'
+
+entrada = Image.open(caminho_entrada)
+saida = remove(entrada)
+
+saida.save(caminho_saida)
